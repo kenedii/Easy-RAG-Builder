de8eebd6fc5af28b965d91a69fa9474155a4e051
kenedii <55466055+kenedii@users.noreply.github.com>
2025-04-17 20:39:04 -0400

    Update launch_api.bat
    Changed port to 8000 for compatibility with frontend.py hardcoded port value

--- launch_api.bat
+++ launch_api.bat
@@ -3,4 +3,4 @@ call api\Scripts\activate            &:: Switches into Virtual Environment to ex
 pip install uv                       &:: Install uv package manager to speed up next command
 uv pip install -r requirements.txt   &:: Installs dependencies with uv package manager
 set KMP_DUPLICATE_LIB_OK=TRUE        &:: API might give errors if this is not set due to duplicate libraries
-uvicorn api:app --reload --port=8888 &:: Launch the FastAPI backend on port 8888
+uvicorn api:app --reload --port=8000 &:: Launch the FastAPI backend on port 8888
