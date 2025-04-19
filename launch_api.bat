eac5b6203d44d260845b755ec95588f4ea7a3609
kenedii <55466055+kenedii@users.noreply.github.com>
2025-04-18 20:01:34 -0400

    Delete launch_api.bat

--- launch_api.bat
+++
@@ -1,6 +0,0 @@
-python -m venv api                   &:: Creates the Python Virtual Environment to run API in 
-call api\Scripts\activate            &:: Switches into Virtual Environment to execute commands
-pip install uv                       &:: Install uv package manager to speed up next command
-uv pip install -r requirements.txt   &:: Installs dependencies with uv package manager
-set KMP_DUPLICATE_LIB_OK=TRUE        &:: API might give errors if this is not set due to duplicate libraries
-uvicorn api:app --reload --port=8000 &:: Launch the FastAPI backend on port 8888
